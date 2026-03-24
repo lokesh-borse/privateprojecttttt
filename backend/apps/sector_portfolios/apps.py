ed841cb03baa5c759c0b4855dafa65c144e42491
Rovin Dcosta <121714118+Rovin1303@users.noreply.github.com>
2026-03-24 10:09:29 +0530

    Delete backend directory

--- backend/apps/sector_portfolios/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SectorPortfoliosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.sector_portfolios'
-    verbose_name = 'Sector Portfolios'
